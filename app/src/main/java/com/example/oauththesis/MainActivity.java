ce74582af9dbe163c7a972b814f117f792e28ce5
Apple <apple@MacBook.wlan>
2022-06-22 19:30:20 +0500

    fireStore added

---
+++ app/src/main/java/com/example/oauththesis/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.oauththesis;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 100;
+    GoogleSignInClient mGoogleSignInClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                String personGivenName = acct.getGivenName();
+                String personFamilyName = acct.getFamilyName();
+                String personEmail = acct.getEmail();
+                String personId = acct.getId();
+                Uri personPhoto = acct.getPhotoUrl();
+
+                Users users=new Users(personName, personGivenName, personFamilyName, personEmail, personId,personPhoto);
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                // Add a new document with a generated ID
+                CollectionReference cities = db.collection("users");
+                cities.document(users.personId).set(users);
+                DocumentReference docRef = db.collection("users").document(personId);
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Log.d("Data", "DocumentSnapshot data: " + document.getData());
+                            } else {
+                                Log.d("No document", "No such document");
+                            }
+                        } else {
+                            Log.d("failed to get", "get failed with ", task.getException());
+                        }
+                    }
+                });
+
+                //   abc =myRef.child(personId).get();
+                Toast.makeText(this, "User email:"+personEmail, Toast.LENGTH_SHORT).show();
+            }
+//            startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            // Signed in successfully, show authenticated UI.
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.d("signInResult:failed code=", e.toString());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/oauththesis/Users.java
@@ -0,0 +1,66 @@
+package com.example.oauththesis;
+
+import android.net.Uri;
+
+public class Users {
+    String personName, personGivenName, personFamilyName, personEmail, personId;
+    Uri personPhoto;
+
+    public Users(String personName, String personGivenName, String personFamilyName, String personEmail, String personId, Uri personPhoto) {
+        this.personName = personName;
+        this.personGivenName = personGivenName;
+        this.personFamilyName = personFamilyName;
+        this.personEmail = personEmail;
+        this.personId = personId;
+        this.personPhoto = personPhoto;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getPersonGivenName() {
+        return personGivenName;
+    }
+
+    public void setPersonGivenName(String personGivenName) {
+        this.personGivenName = personGivenName;
+    }
+
+    public String getPersonFamilyName() {
+        return personFamilyName;
+    }
+
+    public void setPersonFamilyName(String personFamilyName) {
+        this.personFamilyName = personFamilyName;
+    }
+
+    public String getPersonEmail() {
+        return personEmail;
+    }
+
+    public void setPersonEmail(String personEmail) {
+        this.personEmail = personEmail;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public Uri getPersonPhoto() {
+        return personPhoto;
+    }
+
+    public void setPersonPhoto(Uri personPhoto) {
+        this.personPhoto = personPhoto;
+    }
+
+}
