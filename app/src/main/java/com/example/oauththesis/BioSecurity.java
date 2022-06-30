6e38eee24202b6d6f5ce39684ec70e0b774147ff
Apple <apple@MacBook.local>
2022-06-30 21:19:20 +0500

    Pin authenticator added

---
+++ app/src/main/java/com/example/oauththesis/BioSecurity.java
@@ -0,0 +1,178 @@
+package com.example.oauththesis;
+
+
+import android.preference.PreferenceManager;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.util.Base64;
+import android.util.Log;
+
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.util.concurrent.Executor;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
+interface CryptoObjectListener {
+    void available(BiometricPrompt.CryptoObject cryptoObject);
+}
+
+public class BioSecurity {
+
+    private CryptoObjectListener listener = null;
+    private MainActivity context = null;
+
+    private static final String SHARED_PREFERENCE_KEY_IV = "iv";
+
+    public BioSecurity(MainActivity context){
+        this.context = context;
+
+        try {
+            generateSecretKey(new KeyGenParameterSpec.Builder(
+                    "KEY_NAME",
+                    KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .setUserAuthenticationRequired(true)
+                    .build());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void encrypt(CryptoObjectListener listener){
+        this.listener = listener;
+        Cipher cipher = null;
+        try {
+            cipher = getCipher();
+            SecretKey secretKey = getSecretKey();
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            PreferenceManager.getDefaultSharedPreferences(context)
+                    .edit().putString(SHARED_PREFERENCE_KEY_IV, Base64.encodeToString(cipher.getIV(), Base64.NO_WRAP))
+                    .apply();
+
+            BiometricPrompt.PromptInfo promptInfo;
+            promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Enter trusty pin for my app")
+                    .setSubtitle("")
+
+                    .setAllowedAuthenticators(BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+                    .build();
+            createBiometricPrompt().authenticate(promptInfo,new BiometricPrompt.CryptoObject(cipher));
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException | UnrecoverableKeyException | KeyStoreException | CertificateException | IOException | InvalidKeyException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void decrypt(CryptoObjectListener listener){
+        this.listener = listener;
+
+
+                Cipher cipher = null;
+                try {
+                    cipher = getCipher();
+                    SecretKey secretKey = getSecretKey();
+
+                    String keyIV = PreferenceManager.getDefaultSharedPreferences(context)
+                            .getString(SHARED_PREFERENCE_KEY_IV, "");
+
+
+                    byte[] iv = Base64.decode(keyIV, Base64.NO_WRAP);
+                    IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+
+                    cipher.init(Cipher.DECRYPT_MODE, secretKey,ivParameterSpec);
+
+                    BiometricPrompt.PromptInfo promptInfo;
+                    promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                            .setTitle("Biometric login for my app")
+                            .setSubtitle("")
+                            .setNegativeButtonText("Cancel")
+                            .build();
+                    createBiometricPrompt().authenticate(promptInfo,new BiometricPrompt.CryptoObject(cipher));
+                } catch (NoSuchPaddingException e) {
+                    e.printStackTrace();
+                } catch (NoSuchAlgorithmException | UnrecoverableKeyException | KeyStoreException | CertificateException | IOException e) {
+                    e.printStackTrace();
+                } catch (InvalidAlgorithmParameterException e) {
+                    e.printStackTrace();
+                } catch (InvalidKeyException e) {
+                    e.printStackTrace();
+                }
+    }
+
+    private BiometricPrompt createBiometricPrompt() {
+
+        Executor executor = ContextCompat.getMainExecutor(context);
+        String TAG = "BIO TAG";
+
+        BiometricPrompt.AuthenticationCallback callback =  new BiometricPrompt.AuthenticationCallback(){
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Log.d(TAG, "$errorCode :: $errString");
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Log.d(TAG, "Authentication failed for an unknown reason");
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Log.d(TAG, "Authentication was successful");
+                listener.available(result.getCryptoObject());
+            }
+
+        };
+
+        //The API requires the client/Activity context for displaying the prompt
+        BiometricPrompt biometricPrompt = new BiometricPrompt(context, executor, callback);
+        return biometricPrompt;
+    }
+
+
+    //// new code added
+    private void generateSecretKey(KeyGenParameterSpec keyGenParameterSpec) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(
+                KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+        keyGenerator.init(keyGenParameterSpec);
+        keyGenerator.generateKey();
+    }
+
+    private SecretKey getSecretKey() throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+        KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+
+        // Before the keystore can be accessed, it must be loaded.
+        keyStore.load(null);
+        return ((SecretKey)keyStore.getKey("KEY_NAME", null));
+    }
+
+    private Cipher getCipher() throws NoSuchPaddingException, NoSuchAlgorithmException {
+        return Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
+                + KeyProperties.BLOCK_MODE_CBC + "/"
+                + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+    }
+}
--- app/src/main/java/com/example/oauththesis/MainActivity.java
+++ app/src/main/java/com/example/oauththesis/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.oauththesis;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.biometric.BiometricPrompt;
+
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -28,13 +32,19 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 100;
     GoogleSignInClient mGoogleSignInClient;
+    private BiometricPrompt.CryptoObject myCrypto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,10 +60,31 @@ public class MainActivity extends AppCompatActivity {
         SignInButton signInButton = findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
 
+        Button decryptButton = findViewById(R.id.decrypt);
+        BioSecurity bioSecurity = new BioSecurity(MainActivity.this);
+
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                signIn();
+
+                bioSecurity.encrypt(new CryptoObjectListener() {
+                    @Override
+                    public void available(BiometricPrompt.CryptoObject cryptoObject) {
+                        myCrypto=cryptoObject;
+                        signIn();
+//                        String plan_string = "This is my string";
+//                        try {
+//                            encryptedInfo = cryptoObject.getCipher().doFinal(
+//                                    plan_string.getBytes(Charset.defaultCharset()));
+//                        } catch (BadPaddingException e) {
+//                            e.printStackTrace();
+//                        } catch (IllegalBlockSizeException e) {
+//                            e.printStackTrace();
+//                        }
+//                        Log.d("MY_APP_TAG", "Encrypted information: " +
+//                                Arrays.toString(encryptedInfo));
+                    }
+                });
             }
         });
     }
@@ -86,7 +117,30 @@ public class MainActivity extends AppCompatActivity {
                 String personId = acct.getId();
                 Uri personPhoto = acct.getPhotoUrl();
 
-                Users users=new Users(personName, personGivenName, personFamilyName, personEmail, personId,personPhoto);
+                byte[] pEmail = new byte[0];
+                byte[] pName = new byte[0];
+                byte[] pGivenName = new byte[0];
+                byte[] pFamilyName = new byte[0];
+                byte[] pID = new byte[0];
+
+                try {
+                    pEmail = myCrypto.getCipher().doFinal(
+                            personEmail.getBytes(Charset.defaultCharset()));
+                    pName = myCrypto.getCipher().doFinal(
+                            personName.getBytes(Charset.defaultCharset()));
+                    pGivenName = myCrypto.getCipher().doFinal(
+                            personGivenName.getBytes(Charset.defaultCharset()));
+                    pFamilyName = myCrypto.getCipher().doFinal(
+                            personFamilyName.getBytes(Charset.defaultCharset()));
+                    pID = myCrypto.getCipher().doFinal(
+                            personId.getBytes(Charset.defaultCharset()));
+
+                } catch (BadPaddingException e) {
+                    e.printStackTrace();
+                } catch (IllegalBlockSizeException e) {
+                    e.printStackTrace();
+                }
+                Users users=new Users(pName.toString(), pGivenName.toString(), pFamilyName.toString(), pEmail.toString(), pID.toString(),personPhoto);
                 FirebaseFirestore db = FirebaseFirestore.getInstance();
                 // Add a new document with a generated ID
                 CollectionReference cities = db.collection("users");
